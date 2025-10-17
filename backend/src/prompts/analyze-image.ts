3e4d4b5124bb5ca15e044ac6e25d1ab0f1efcb57
Mario Trzensky <mario.trzensky@cologne-intelligence.de>
2025-10-17 09:36:32 +0200

    Check for isCameraActive for capture ability

--- backend/src/prompts/analyze-image.ts
+++ backend/src/prompts/analyze-image.ts
@@ -1,11 +1,13 @@
 import { getTypeScriptInterfacesAsString } from "../functions/get-typescript-interfaces";
 
-export const analyzeImagePrompt = () => `You receive a base64-encoded image (as data). You will identify and extract the following fields based on these interface definitions:
+export const analyzeImagePrompt = () => `You receive a base64-encoded image (as data). You will identify and extract the following properties based on these interface definitions:
 ${getTypeScriptInterfacesAsString()}
+Very important: Leave all fields empty that cannot be determined from the image, only make assumptions about visual identifiable properties.
 
-Focus on the person identified in the image.
-Leave all fields empty that cannot be determined from the image, only make assumptions about visual identifiable properties.
-Never fill fields relevant to the image content with random or made-up data.
+Never fill JSON properties relevant to the image content with random or made-up data.
+Focus on the person identified in the image and assume properties based on what you can see.
 Only information around that person in the image can be added to "notes".
+Never fill JSON property "notes" with "image not clear" or similar, if no additional information is available for "notes", don't fill it.
 Do not include any additional commentary or text outside of the JSON object.
-Return JSON as provided by above type definitions.`
+NEVER fill 'firstname' or 'lastname'. Leave them empty.
+Return JSON as provided by above type definitions and leave not clear identifiable properties inside the object empty.`
