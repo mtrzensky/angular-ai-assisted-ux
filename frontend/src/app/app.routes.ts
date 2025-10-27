9b88be06dda963835626a01d9ab3f66b5e050a62
Mario Trzensky <mario.trzensky@cologne-intelligence.de>
2025-10-27 19:45:09 +0100

    Remove navigation structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { RegistrationFormComponent } from './components/registration/registration-form.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'registration', pathMatch: 'full' },
-  { path: 'registration', component: RegistrationFormComponent },
+  { path: '', redirectTo: 'profiling', pathMatch: 'full' },
   { path: 'profiling', component: RegistrationFormComponent },
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,31 +1,12 @@
-import { Component, signal, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule,
-    MatIconModule,
-    MatButtonModule,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  @ViewChild('drawer') sidenav!: MatSidenav;
-
-  toggleSidenav() {
-    this.sidenav.toggle();
-  }
-}
+export class App {}
